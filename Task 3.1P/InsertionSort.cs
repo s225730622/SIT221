1f32de7a8f5293269db1a542a1b5e677195d5933
s225730622 <s225730622@deakin.edu.au>
2026-03-28 01:22:32 +1100

    Update InsertionSort.cs

--- Task 3.1P/InsertionSort.cs
+++ Task 3.1P/InsertionSort.cs
@@ -7,6 +7,18 @@ namespace SIT221_Library
     {
         public void Sort<K>(K[] array, int index, int num, IComparer<K> comparer) where K : IComparable<K>
         {
+            // Throw exception error if array is null
+            if (array == null) 
+                throw new ArgumentNullException(nameof(array));
+
+            // Throw error if index or num or both are negative
+            if (index < 0 || num < 0) 
+                throw new ArgumentOutOfRangeException();
+
+            // Throw error if index & num values do not specify a valid range within array
+            if (index + num > array.Length) 
+                throw new ArgumentException();
+
             for (int i = index + 1; i < index + num; i++)
             {
                 K temp = array[i];
